2265126ddc9bf11596f3dfba29505af9e7dff800
Renan Hilario <rvahilario@gmail.com>
2022-05-04 01:35:29 -0300

    feat: :star2: add examples components using tailwind + styled-comp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,45 @@
 import Head from 'next/head';
+import tw from 'twin.macro';
+import styled from 'styled-components';
 // import styles from '@/styles/';
 
 import { ExampleComponent } from '@/components/ExampleComponent';
+const StyledButton = styled.button`
+	background: red;
+	color: white;
+	font-size: 1em;
+	text-align: center;
+	padding: 0.25em 1em;
+	border: 2px solid black;
+`;
+
+const TailwindButton = tw.button`
+  bg-red-500
+  hover:bg-red-700
+  text-white
+  font-bold
+  py-2
+  px-4
+  border
+  border-black
+  rounded
+`;
+
+const ConditionalButton = styled.button<{ isRed: boolean }>`
+	${({ isRed }) =>
+		isRed
+			? tw`bg-red-500 hover:bg-red-700`
+			: tw`bg-blue-500 hover:bg-blue-500`};
+	${tw`
+		text-white
+    font-bold
+    py-2
+    px-4
+    border
+    border-black
+    rounded
+	`}
+`;
 
 export default function Home() {
 	return (
@@ -9,10 +47,18 @@ export default function Home() {
 			<Head>
 				<title>'TypeScript starter for Next.js'</title>
 			</Head>
-
-			<h1>~ Hello World ~</h1>
-
-			<ExampleComponent title="Example button" />
+			<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+				<ExampleComponent title="Example" />
+				<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+					<StyledButton>In Style</StyledButton>
+					<br />
+					<TailwindButton>In Tailwind Style</TailwindButton>
+					<br />
+					<ConditionalButton isRed={false}>
+						Conditional Tailwind
+					</ConditionalButton>
+				</main>
+			</main>
 		</>
 	);
 }
