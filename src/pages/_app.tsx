6f825c89804fa89abf194036033857e85b4b82e9
Renan Hilario <rvahilario@gmail.com>
2022-05-04 01:31:58 -0300

    config: :wrench: add tailwind configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from 'next/app';
 import Theme from '@/styles/theme';
+import Head from 'next/head';
+import 'tailwindcss/tailwind.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Theme>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
 			<Component {...pageProps} />
 		</Theme>
 	);
