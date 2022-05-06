fd70887ac7092177424fbc8064e68a65411731ba
Renan Hilario <rvahilario@gmail.com>
2022-05-06 01:44:06 -0300

    feat: :star2: add header and footer to WidgetForm

--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -1,10 +1,14 @@
 import { Container } from './styles';
+import { FooterWidget } from '../FooterWidget';
+import { HeaderWidget } from '../HeaderWidget';
 import { WidgetFormProps } from './types';
 
 export const WidgetForm = ({}: WidgetFormProps) => {
 	return (
-		<Container>
-			<h1>WidgetForm</h1>
+		<Container className="w-[calc(100vw-2rem)]">
+			<HeaderWidget title="Send your feedback" />
+
+			<FooterWidget />
 		</Container>
 	);
 };
