357b10a61e24b0386435c6274b5ee30bbfb86d5e
Renan Hilario <rvahilario@gmail.com>
2022-05-06 01:44:31 -0300

    style: :art: improving WidgetForm styles

--- src/components/WidgetForm/styles.ts
+++ src/components/WidgetForm/styles.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
+import tw from 'twin.macro';
 
 export const Container = styled.div`
-	background-color: aqua;
+	${tw`
+		bg-surface-primary
+		p-4
+		relative
+		rounded-2xl
+		mb-4
+		flex-col
+		items-center
+		shadow-lg
+		md:w-auto
+		`}
+`;
+
+export const FormButtons = styled.div`
+	${tw`
+		py-8
+		gap-2
+		w-full
+		flex
+	`}
 `;
