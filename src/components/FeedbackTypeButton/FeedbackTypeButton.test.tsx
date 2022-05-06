f4ee55437be0a1783babf509f32f8922bef8bc25
Renan Hilario <rvahilario@gmail.com>
2022-05-06 02:57:44 -0300

    test: :white_check_mark: updating FeedbackTypeButton test

--- src/components/FeedbackTypeButton/FeedbackTypeButton.test.tsx
+++ src/components/FeedbackTypeButton/FeedbackTypeButton.test.tsx
@@ -3,10 +3,18 @@ import { FeedbackTypeButton } from './FeedbackTypeButton';
 
 describe('<FeedbackTypeButton />', () => {
 	it('should render FeedbackTypeButton', () => {
-		render(<FeedbackTypeButton />);
+		render(
+			<FeedbackTypeButton
+				title="TitleBUG"
+				srcImage="/bug.svg"
+				altImage="Image of a bug."
+			/>,
+		);
 
-		const feedbackTypeButton = screen.getByText('FeedbackTypeButton');
+		const feedbackTypeButton = screen.getByRole('button', { name: /TitleBUG/ });
+		const imageButton = screen.getByRole('img', { name: 'Image of a bug.' });
 
 		expect(feedbackTypeButton).toBeInTheDocument();
+		expect(imageButton).toBeInTheDocument();
 	});
 });
