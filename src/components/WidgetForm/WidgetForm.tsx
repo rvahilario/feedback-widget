b2de73e211d9cb450e56fd6adb666ed67822e5c0
Renan Hilario <rvahilario@gmail.com>
2022-05-06 01:48:39 -0300

    feat: :star2: created feedbackTypes and render Buttons from it

--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -1,12 +1,25 @@
-import { Container } from './styles';
+import { feedbackTypes } from '@/utils/utils';
+import { FeedbackTypeButton } from '../FeedbackTypeButton';
 import { FooterWidget } from '../FooterWidget';
 import { HeaderWidget } from '../HeaderWidget';
+import { Container, FormButtons } from './styles';
 import { WidgetFormProps } from './types';
 
 export const WidgetForm = ({}: WidgetFormProps) => {
 	return (
 		<Container className="w-[calc(100vw-2rem)]">
 			<HeaderWidget title="Send your feedback" />
+
+			<FormButtons>
+				{Object.entries(feedbackTypes).map(([key, value]) => (
+					<FeedbackTypeButton
+						key={key}
+						title={value.title}
+						srcImage={value.image.source}
+						altImage={value.image.alt}
+					/>
+				))}
+			</FormButtons>
 
 			<FooterWidget />
 		</Container>
---
+++ src/utils/utils.tsx
@@ -0,0 +1,23 @@
+export const feedbackTypes = {
+	BUG: {
+		title: 'Bug',
+		image: {
+			source: '/bug.svg',
+			alt: 'Image of a bug.',
+		},
+	},
+	IDEA: {
+		title: 'Idea',
+		image: {
+			source: '/idea.svg',
+			alt: 'Image of a light bulb.',
+		},
+	},
+	OTHER: {
+		title: 'Other',
+		image: {
+			source: '/thought.svg',
+			alt: 'Image of a thought cloud',
+		},
+	},
+};
